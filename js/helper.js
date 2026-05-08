580bd90e90f51396168d13435763cb316d76e424
indigoxela <user@olga.indigofloat.at>
2026-05-08 12:13:04 +0200

    Stop using deprecated click shorthand

--- js/helper.js
+++ js/helper.js
@@ -1,7 +1,7 @@
 (function ($) {
   "use strict";
   $(document).ready(function() {
-    $('.menu-toggle-button, .l-header a').click(function () {
+    $('.menu-toggle-button, .l-header a').on('click', function () {
       $('.l-header').toggleClass('nav-visible');
     });
   });
